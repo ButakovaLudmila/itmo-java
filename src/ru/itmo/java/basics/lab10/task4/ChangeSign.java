c0f05326f180f023b6ca6c7151977ac141d29012
ButakovaLudmila <ludmila.butakova@gmail.com>
2023-11-20 18:00:53 +0300

    Lab10

--- src/ru/itmo/java/basics/lab10/task4/ChangeSign.java
+++ src/ru/itmo/java/basics/lab10/task4/ChangeSign.java
@@ -4,19 +4,20 @@ import java.io.*;
 
 public class ChangeSign {
     File cf = new File("files/text4.txt");
-
-
+    //char symbol = '$';
+    String symbol="\\$";
     public void printThis() {
         try {
             FileReader fr = new FileReader(cf);
             String s;
             String str = "";
-            char symbol = 'S';
+
+
             try (BufferedReader br = new BufferedReader(fr)) {
                 while ((s = br.readLine()) != null) {
                     str += s;
                 }
-                str = str.replaceAll("[^a-zA-Z ]", String.valueOf(symbol));
+                str = str.replaceAll("[^a-zA-Zа-яА-Я]", symbol);
                 FileWriter fw = new FileWriter(cf);
                 fw.write(str);
                 fw.close();
@@ -24,6 +25,7 @@ public class ChangeSign {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
